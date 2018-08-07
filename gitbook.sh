e23f32018fb54fc776a7a559c9cf124d7e4bb0d4
yangjh <yangjh@yeah.net>
2018-08-07 14:12:41 +0800

    增加写出我心笔记

--- gitbook.sh
+++ gitbook.sh
@@ -3,10 +3,3 @@ gitbook build
 git add .
 git commit -m $1
 git push -u origin master
-git checkout pages
-cp -r _book/* .
-rm References.md
-git add .
-git commit -m $1
-git push -u origin pages
-git checkout master
