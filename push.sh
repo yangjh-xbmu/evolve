a41dc2364c2907f4a12b914b9b966042bc57d250
yangjh <yangjh@yeah.net>
2019-04-21 22:38:21 +0800

    增加github仓库

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@ gitbook build
 git add .
 git commit -m $1
 git push -u origin master
-git push -u origin hub
+git push -u hub master
 open https://yangjh.gitee.io/evolve
 open https://github.com/yangjh2019
